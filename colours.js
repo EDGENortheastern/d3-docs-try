e1e9cc542fce2ba504b15be2a444e8aafb4c00ea
Katia <katia.punter@storyshareplatform.com>
2025-11-01 14:38:24 +0000

    nul sec branding colours

---
+++ colours.js
@@ -0,0 +1,8 @@
+// colours.js
+// Index 0 = lightest shade, index 4 = darkest shade
+const colours = {
+  peach: ["#faae80", "#f79868", "#f58552", "#e46225", "#d74b19"],
+  yellow: ["#ffe1a5", "#ffd580", "#fec54c", "#fdb737", "#e2a854"],
+  green: ["#bfe2c6", "#98d1b5", "#609f80", "#045944", "#1b4538"],
+  blue: ["#c8eaf7", "#a3dcec", "#62b6d0", "#2d7496", "#0d3353"]
+};
